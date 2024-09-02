efdd992b5bb60b6c6f51066c22c78fc81ce28d88
Kennedyongogo <ongogokennedy89@gmail.com>
2024-09-02 14:50:30 +0300

    update customer billing

--- src/models/CustomerBilling.js
+++ src/models/CustomerBilling.js
@@ -8,35 +8,15 @@ module.exports = (sequelize) => {
       allowNull: false,
       primaryKey: true,
     },
-    Amount: {
-      type: DataTypes.DECIMAL,
-      allowNull: true,
-    },
-    CurrentBalance: {
-      type: DataTypes.DECIMAL,
-      allowNull: true,
-    },
-    PreviousBalance: {
-      type: DataTypes.DECIMAL,
-      allowNull: true,
-    },
-    CreatedDate: {
+    OldAcc: {
       type: DataTypes.STRING,
       allowNull: true,
-    },
-    DueDate: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    Phone: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    Account: {
-      type: DataTypes.STRING,
-      allowNull: false,
     },
     Name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Acc_No: {
       type: DataTypes.STRING,
       allowNull: true,
     },
@@ -44,27 +24,111 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    Scheme: {
+    Sub_Zone: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    Route: {
+    Estimate: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    MeterStatus: {
+    Period: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    AccountStatus: {
+    Water: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    Sewer: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    Meter_No: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    MeterSize: {
+    Tarrif: {
       type: DataTypes.STRING,
       allowNull: true,
     },
-    ParcelNo: {
+    Consumption: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    C_Date: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    C_Read: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    P_Date: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    P_Read: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    m_Total: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    Bill_No: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Due_Date: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    B_Date: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Con_P: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    CuttOff: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    mDesc: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    mAmount: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    Arrears: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    Est: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    sConsumption: {
+      type: DataTypes.NUMBER,
+      allowNull: true,
+    },
+    MState: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    BAmount: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    Printed: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    R_Date: {
       type: DataTypes.STRING,
       allowNull: true,
     },
