1bd8b9f9001f8a6cdc9a0107f735febf6e547aac
mutetid <mutetidanz@gmail.com>
2024-09-09 17:16:40 +0300

    updates

--- src/models/Boreholes.js
+++ src/models/Boreholes.js
@@ -1,63 +1,57 @@
 const Sequelize = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  const Boreholes = sequelize.define(
-    "Boreholes",
-    {
-      ID: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
-      },
-      Longitude: {
-        type: DataTypes.DOUBLE,
-        allowNull: true,
-      },
-      Latitude: {
-        type: DataTypes.DOUBLE,
-        allowNull: true,
-      },
-      geom: {
-        type: DataTypes.GEOMETRY("POINT", 4326),
-        allowNull: true,
-      },
-      Yield: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      PipeDiameter: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      Zone: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      CasingDiameter: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      YearOfInstallation: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      Status: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      Remarks: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      Photo: {
-        type: DataTypes.STRING(254),
-        allowNull: true,
-      },
+  const Boreholes = sequelize.define("Boreholes", {
+    ID: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
     },
-    {
-      paranoid: true, 
-    }
-  );
+    Longitude: {
+      type: DataTypes.DOUBLE,
+      allowNull: true,
+    },
+    Latitude: {
+      type: DataTypes.DOUBLE,
+      allowNull: true,
+    },
+    geom: {
+      type: DataTypes.GEOMETRY("POINT", 4326),
+      allowNull: true,
+    },
+    Yield: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    PipeDiameter: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Zone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    CasingDiameter: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    YearOfInstallation: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Status: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Remarks: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    Photo: {
+      type: DataTypes.STRING(254),
+      allowNull: true,
+    },
+  });
 
   return Boreholes;
 };
