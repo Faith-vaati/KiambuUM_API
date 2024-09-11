494ecc98bc1e5f1cf11c598f0b88f69ba9ab8cdb
Kennedyongogo <ongogokennedy89@gmail.com>
2024-09-11 17:02:39 +0300

    TANKS  TEXT

--- src/models/Tanks.js
+++ src/models/Tanks.js
@@ -66,7 +66,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: true,
     },
     Photo: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: true,
     },
     geom: {
