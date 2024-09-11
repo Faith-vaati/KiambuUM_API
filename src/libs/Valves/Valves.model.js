31ab5b65cf0b653482514a7e41147895663fd34d
Kennedyongogo <ongogokennedy89@gmail.com>
2024-09-11 17:14:32 +0300

    create valves

--- src/libs/Valves/Valves.model.js
+++ src/libs/Valves/Valves.model.js
@@ -21,7 +21,7 @@ exports.createValve = (ValvesData) => {
         try {
           const id = result.dataValues.ID;
           const [data, dmeta] = await sequelize.query(
-            `UPDATE public."Valves" SET "geom" = ST_SetSRID(ST_MakePoint("Longitude", "Latitude"), 4326) WHERE "ID" = '${id}';`
+            `UPDATE public."Valves" SET "geom" = ST_SetSRID(ST_MakePoint("Longitude", "Latitude", 0, 0), 4326) WHERE "ID" = '${id}';`
           );
           resolve({
             success: "Created successfully",
